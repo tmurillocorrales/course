800627759950edbd282f7a6b7923c11c359b8ae2
Tatiana_Murillo <tmurill@gwdg.com>
2020-06-16 08:48:59 +0000

    correct file

--- clean_code/gitTutorial/myFirstGit/randomNumber.py
+++ clean_code/gitTutorial/myFirstGit/randomNumber.py
@@ -18,9 +18,14 @@ def write_log_file(outputfilename, data):
     f.close()
 
 
+def get_color_by_dice_roll(spots):
+    colors = ["blue", "green", 'red', 'yellow', 'purple', 'orange']
+    return colors[spots-1]
+
+
 if __name__ == "__main__":
-    outputfilename = "numbers"
+    outputfilename = "randomNumber"
     roll = get_random_number(1, 6)
-    get_color_by_dice_roll = roll
-    write_log_file(outputfilename, roll)
-    print(“debug print”)
+    color = get_color_by_dice_roll(roll)
+    write_log_file(outputfilename, color)
+
