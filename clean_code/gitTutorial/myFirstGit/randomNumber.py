10bb70daeca598ab5e53b4ca45d90a2a61525c71
Tatiana_Murillo <tmurill@gwdg.com>
2020-06-16 07:44:29 +0000

    replaced my with our

--- clean_code/gitTutorial/myFirstGit/randomNumber.py
+++ clean_code/gitTutorial/myFirstGit/randomNumber.py
@@ -14,7 +14,7 @@ def get_random_number(start, end):
 
 def write_log_file(outputfilename, data):
     f = open(outputfilename + ".log", "a")
-    f.write("My randomly generated number is " + str(data) + " (" + time.strftime("%H:%M:%S") + ")\n")
+    f.write("Our randomly generated number is " + str(data) + " (" + time.strftime("%H:%M:%S") + ")\n")
     f.close()
 
 
